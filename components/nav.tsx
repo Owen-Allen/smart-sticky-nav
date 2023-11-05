18e5aa9a90042636fe7d22f44bae5574203c08fa
Owen-Allen <73844263+Owen-Allen@users.noreply.github.com>
2023-11-04 22:26:37 -0400

    refactored to simplify handleScroll logic. removed unecessary tailwind classes. added constants to control height and minimum y position.

--- components/nav.tsx
+++ components/nav.tsx
@@ -6,52 +6,56 @@ import Dropdown from "@/components/dropdown";
 import LogoLink from "@/components/logoLink";
 import CluckButton from "@/components/cluckButton";
 
+const NAV_HEIGHT = 80;
+const MIN_TOP = -80; // minimum y position used to fully hide the navbar above the viewport on scrolldown
+
 const Nav = () => {
-  const [top, setTop] = useState(0); // is always between -80 and 0
-  const prevScrollPos = useRef(0);
+    const [top, setTop] = useState<number>(0); // always between MIN_TOP and 0
+    const previousPosition = useRef<number>(0);
 
-  const handleScroll = (e: any) => {
-    const currentScrollPos = window.scrollY;
-    if (currentScrollPos > prevScrollPos.current) {
-      // user scrolled down
-      let change = currentScrollPos - prevScrollPos.current;
-      let newMarginTop = top - change;
-      if (newMarginTop < -80) {
-        newMarginTop = -80;
-      }
-      setTop(newMarginTop);
-    } else if (currentScrollPos < prevScrollPos.current) {
-      // user scrolled up
-      let change = prevScrollPos.current - currentScrollPos;
-      let newMarginTop = top + change;
-      if (newMarginTop > 0) {
-        newMarginTop = 0;
-      }
-      setTop(newMarginTop);
-    }
-    prevScrollPos.current = currentScrollPos;
-  };
+    const handleScroll = () => {
+        const currentPosition = window.scrollY;
 
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll, { passive: true });
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
+        // change > 0 -> user scrolled down, change < 0 -> user scrolled up
+        const change = currentPosition - previousPosition.current;
+
+        // move nav to be opposite of the change
+        let newTop = top - change;
+
+        if (newTop > 0) {
+            // do not let the navbar slide too far down the page
+            newTop = 0;
+        } else if (newTop < MIN_TOP) {
+            // do not let the navbar slide too far above the page
+            newTop = MIN_TOP;
+        }
+        setTop(newTop);
+        previousPosition.current = currentPosition;
     };
-  });
 
-  return (
-    <nav
-      className={`sticky z-50 flex h-20 w-full bg-sky-400`}
-      style={{ top: `${top}px` }}
-    >
-      <div className="mx-4 flex w-full items-center sm:mx-16">
-        <Dropdown />
-        <LogoLink className="-mr-12 ml-auto w-12" />{" "}
-        {/* -mr must match w to be centered correctly */}
-        <CluckButton />
-      </div>
-    </nav>
-  );
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll, { passive: true });
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    });
+
+    return (
+        <nav
+            className={`sticky z-50 flex w-full bg-sky-400`}
+            style={{
+                top: `${top}px`,
+                height: `${NAV_HEIGHT}px`,
+            }}
+        >
+            <div className="mx-4 flex w-full items-center sm:mx-16">
+                <Dropdown />
+                <LogoLink className="-mr-12 ml-auto w-12" />
+                {/* -mr must match w to be centered correctly */}
+                <CluckButton />
+            </div>
+        </nav>
+    );
 };
 
 export default Nav;
